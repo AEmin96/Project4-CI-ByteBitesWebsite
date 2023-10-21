965e7d399e1f43f3601c943f9cd0fbf20530a259
AEmin96 <>
2023-10-21 13:23:53 +0000

    update urls in bookings

--- bookings/urls.py
+++ bookings/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     
     path('book/', views.book, name='book'),
-    path('mybookings', views.mybookings, name='mybookings')
+    path('mybookings', views.mybookings, name='mybookings'),
+    path('booking/<int:booking_id>/update/', views.update_booking, name='update_booking'),
+    path('booking/<int:booking_id>/delete/', views.delete_booking, name='delete_booking'),
+
 ]
