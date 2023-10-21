f93d5b98494e8a25268544c48af58fb0047525f7
AEmin96 <>
2023-10-21 11:05:56 +0000

    show username with date in admin

--- bookings/admin.py
+++ bookings/admin.py
@@ -2,3 +2,9 @@ from django.contrib import admin
 from .models import Booking
 
 admin.site.register(Booking)
+
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ['username', 'date']
+
+
+admin.site.register(Booking, BookingAdmin)
